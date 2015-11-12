ae6b387be69bb2dd4e60a5593ea9941580ccaa06
geanes <glenn.eanes@gmail.com>
2015-11-12 14:28:37 -0600

    Initial DESCRIPTION and kidnapr.R commit.

---
+++ R/kidnapr.R
@@ -0,0 +1,25 @@
+#' kidnapr: Shiny application to collect data for future inclusion in the
+#' kidstats package.
+#'
+#' \code{kidnapr} Purpose built Shiny application to collect data.
+#'
+#' @docType package
+#' @name kidnapr
+NULL
+
+#' @import shiny
+NULL
+
+#' @import shinydashboard
+NULL
+
+#' @import shinyjs
+NULL
+
+#' KidnapR shiny app call
+#' @examples
+#' kidnapr()
+#' @export
+kidnapr <- function(){
+  shiny::runApp(system.file('app', package = 'kidnapr'), launch.browser = TRUE)
+}
